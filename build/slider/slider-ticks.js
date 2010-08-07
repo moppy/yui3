cce49e577e466f79f9c4b99efc57ea2f02d0c92a
moppy <spam@mtk.co.il>
2010-08-07 19:55:04 +0300

    Y axis support + PNG support + using updated imgLoader
    Signed-off-by: moppy <spam@mtk.co.il>

--- build/slider/slider-ticks.js
+++ build/slider/slider-ticks.js
@@ -4,7 +4,7 @@ TickSlider
 version: 1
 */
 
-YUI({ debug : true }).add("slider-ticks", function ( Y ) {
+YUI().add("slider-ticks", function ( Y ) {
  
     // Define a new extension class to calculate values differently
     function TickSlider() {
@@ -32,27 +32,39 @@ YUI({ debug : true }).add("slider-ticks", function ( Y ) {
         /* additional prototype members and methods */          
         prototype: { 
             _onRenderAddTicks: function( e ) 
-            {
-                var sThumbUrl = this.get( 'thumbUrl' );
-                //png and ie6 issue...
-                sThumbUrl = sThumbUrl.replace(/[^\/]*\..{3,4}$/, "tick.png" );            
-                var oSlide = Y.Node.getDOMNode( e.parentNode );
+            {                
+                var oSlide = oSlideParent = Y.Node.getDOMNode( e.parentNode );
                 var nTicks =  parseInt( this.get( 'ticks' ) );
-                var nPos;                     
-                for(i = 0; i < nTicks; i++) {                
-                    oTick = Y.DOM.create( "<div id='tick" + i + "'></div>" );            
-                    Y.DOM.addClass( oTick, "tick" );       
-                    Y.DOM.setStyle( oTick, "width", this.get( 'length' ) );
-                    nPos  = i * 100 / ( nTicks - 1 );                        
+                var nPos, sId, imgLoader, sTickUrl, parsePng;
+                var sBackgroundPosition = (this._key.xyIndex) ? "0% ":"";                                      
+                for(i = 0; i < nTicks; i++) {
+                    sId = "tick" + i;
+                    oTick = Y.DOM.create( "<div id='tick" + i + "' class='yui3-slider-tick-" + this.axis + "'></div>" );                   
+                    Y.DOM.addClass( oTick, "tick" );
+                    Y.DOM.setStyle( oTick, this._key.dim, this.get( 'length' ) );
+                    nPos  = i * 100 / ( nTicks - 1 );
                     nPos += ( 5 - i * 10 / ( nTicks - 1) );
-                    Y.DOM.setStyle( oTick, "backgroundPosition", nPos + "%" );                    
+                    Y.DOM.setStyle( oTick, "backgroundPosition", sBackgroundPosition + nPos + "%" );                    
                     Y.DOM.setStyle( oTick, "backgroundRepeat", "no-repeat" );
-                    Y.DOM.setStyle( oTick, "backgroundImage", "url(" + sThumbUrl + ")" );      
                     Y.DOM.addHTML( Y.DOM.elementByAxis( oSlide, "parentNode" ), oTick );   
                     Y.DOM.addHTML( oTick, oSlide );
                     //each time insert to the parent of the previous tick
                     oSlide = oTick;
-                }                            
+                }
+
+                //png support: if the tick image is png, assume the rest may be as well
+                sTickUrl = Y.DOM.getStyle(oTick, "backgroundImage");
+                //use imageLoader to test for png parsing
+                imgLoader = new Y.ImgLoadImgObj ({domId: sId, bgUrl: sTickUrl, isPng: true});
+                parsePng = imgLoader.get( "isPng" );
+                if (parsePng)
+                { 
+                    //parse all images inside the tick meaning the slider
+                    nodeList = Y.Selector.query("*", oTick, false, true);
+                    //add the current tick itself
+                    nodeList[nodeList.length] = oTick;
+                    this._pngParse( nodeList );
+                }
             },
             
            /**
@@ -67,7 +79,7 @@ YUI({ debug : true }).add("slider-ticks", function ( Y ) {
                 this.evtValueChanged = this.after( "valueChange", this._afterValueChange );
             },
            
-            /**
+           /**
             * Unbinding the valueChange logic. In combine with the _bindValueLogic() 
             * can fix 
             * trigger UI and related state updates.
@@ -99,12 +111,38 @@ YUI({ debug : true }).add("slider-ticks", function ( Y ) {
              * @param e { EventFacade } The <code>valueChange</code> event.
              * @protected
              */            
-            _afterValueChange: function( e )
+            _afterValueChange: function(e)
             {       
                 //operate nicely inside a sandbox        
                 this._unBindValueLogic();
                 this._setPosition(this._nearestTick(e.newVal));   
                 this._bindValueLogic();                
+            },
+
+           /**
+            * Parse nodes that may have png images and support 
+            * png for browsers which have problem with png transparency
+            *           
+            * @param {Array} array of {HTMLElement}              
+            * @method _pngParse
+            * @protected             
+            */
+            _pngParse: function(nodeList)
+            {
+                var imgUrl, isPng, imgLoader, id;
+                for(var i = 0; i < nodeList.length; i++){
+                    imgUrl = Y.DOM.getStyle(nodeList[i], "backgroundImage");
+                    id = nodeList[i].id;
+                    isPng = /\.png\)?$/.test(imgUrl);
+                    //exploiting imgLoader for pngStyling                                        
+                    imgLoader = new Y.ImgLoadImgObj ({domId: id, bgUrl: imgUrl, isPng: isPng});
+                    node = Y.one(nodeList[i]);
+                    if (isPng)
+                    {
+                        Y.DOM.setStyle(node, "backgroundImage", "none");
+                        imgLoader.addPngStyle(node);
+                    }
+                }
             },
 
             /**
@@ -134,4 +172,4 @@ YUI({ debug : true }).add("slider-ticks", function ( Y ) {
     
     
      
-}, '3.1.1' ,{requires:['slider-base','clickable-rail', 'slider-value-range']});
+}, '3.1.1' ,{requires:['slider-base','clickable-rail', 'slider-value-range', 'imageloader']});
